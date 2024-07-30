fe648cd42c2065c9a9fd5fd6c52fc424c03e3043
yes_jette <jette.thiele@rl-institut.de>
2024-07-30 14:57:14 +0200

    xx

--- komm_wert_django/komm_wert_django/settings.py
+++ komm_wert_django/komm_wert_django/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = '/kommWertTool/static/'
 
 # Location where collectstatic will collect static files for production
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
