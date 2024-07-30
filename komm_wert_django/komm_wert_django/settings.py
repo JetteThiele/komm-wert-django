86d8e91d6e8864f852d31d89a41a610b216b99dd
yes_jette <jette.thiele@rl-institut.de>
2024-07-30 11:22:12 +0200

    xx

--- komm_wert_django/komm_wert_django/settings.py
+++ komm_wert_django/komm_wert_django/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "komm_wert_django.kommWertTool"
+    "kommWertTool"
 ]
 
 MIDDLEWARE = [
