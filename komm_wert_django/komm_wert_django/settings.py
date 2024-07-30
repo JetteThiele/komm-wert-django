364f7b23241f83be1a87b5cd8a6dead513850615
yes_jette <jette.thiele@rl-institut.de>
2024-07-30 10:11:54 +0200

    xx

--- komm_wert_django/komm_wert_django/settings.py
+++ komm_wert_django/komm_wert_django/settings.py
@@ -116,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Stelle sicher, dass STATIC_URL immer noch konfiguriert ist
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
